7f0ce828a1c83360e83a47b6a87263a783de56b1
Rudeus <89884024+Perugius@users.noreply.github.com>
2024-01-16 12:32:35 +0100

    Add files via upload

---
+++ udp_test.py
@@ -0,0 +1,17 @@
+import socket
+
+# Define the IP address and port of the computer
+computer_ip = "192.168.158.43"
+computer_port = 5005	  # Choose any available port number
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+message = 'test'
+
+while True:
+	f = open("data_trans.txt", "r")
+	message = f.read()
+	f.close()
+	print(message)
+	# Send the message to the computer
+	sock.sendto(message.encode(), (computer_ip, computer_port))
