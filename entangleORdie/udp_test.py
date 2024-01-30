5e908f4f1e71bb5bca9cdc714de6e67dfa60e80f
Rudeus <89884024+Perugius@users.noreply.github.com>
2024-01-30 09:24:12 +0100

    Add files via upload

---
+++ entangleORdie/udp_test.py
@@ -0,0 +1,34 @@
+import flet as ft
+import time
+import socket
+
+# Define the IP address and port to listen on
+computer_ip = "192.168.178.20"
+computer_port = 5005
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the address
+sock.bind((computer_ip, computer_port))
+
+def main(page: ft.Page):
+    n = 0
+
+    t = ft.Text(color="white", size=150, weight=ft.FontWeight.W_200)
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(t)
+
+    while True:
+        # Receive data
+        data, addr = sock.recvfrom(1024)
+        message = data.decode()
+        print(message)
+        t.value = message
+        n += 1
+        if n >= 500:
+            n = 0
+            page.update()
+
+ft.app(target=main)
